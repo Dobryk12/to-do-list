1382c8abcc21a400b53b5d7dec1a1c2d23d00b53
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-03-11 13:49:45 +0200

    fix bugs

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "crispy_forms",
-    # "crispy_bootstrap4"
+    "crispy_bootstrap4",
     "list"
 ]
 
@@ -125,5 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
--- list/urls.py
+++ list/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from list.views import index, TagListView, TagCreateView, TagUpdateView, TagDeleteView
+from list.views import index, TagListView, TagsCreateView, TagsUpdateView, TagsDeleteView
 
 urlpatterns =[
     path("", index, name="index"),
     path("tags/", TagListView.as_view(), name="tag-list"),
-    path("tags/create", TagCreateView.as_view(), name="tag-create"),
-    path("tags/<int:pk>/update", TagUpdateView.as_view(), name="tag-update"),
-    path("tags/<int:pk>/delete", TagDeleteView.as_view(), name="tag-delete")
+    path("tags/create", TagsCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update", TagsUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete", TagsDeleteView.as_view(), name="tag-delete")
 ]
